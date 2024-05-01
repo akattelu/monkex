4aac9aa7819737df2830926a1352b80babd5ad47
Aakash Kattelu <aakash@goforward.com>
2024-05-01 14:18:04 -0700

    Add failing evaluator test for integer literals

---
+++ test/evaluator_test.exs
@@ -0,0 +1,25 @@
+defmodule EvaluatorTest do
+  use ExUnit.Case
+  alias Monkex.Object
+  alias Monkex.Object.Node
+  alias Monkex.Lexer
+  alias Monkex.Parser
+
+  defp eval(input) do
+    {parser, program} = input |> Lexer.new() |> Parser.new() |> Parser.parse_program()
+    assert parser.errors == []
+    program |> Node.eval()
+  end
+
+  test "evaluate integers" do
+    cases = [
+      {"5", 5},
+      {"10", 10}
+    ]
+
+    cases
+    |> Enum.each(fn {input, expected} ->
+      assert input |> eval == expected
+    end)
+  end
+end
