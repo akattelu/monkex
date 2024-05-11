4263109b310b75f2292c81f2fb8509822a59f968
Aakash Kattelu <aakash@goforward.com>
2024-05-11 16:15:20 -0700

    Create mix mx task to run on a file

---
+++ lib/mix/tasks/eval.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Mx do
+  @moduledoc "Run the interpreter against a file\r\n "
+  @shortdoc "Run the interpreter against a file"
+  @requirements ["app.config"]
+
+  use Mix.Task
+
+  @impl Mix.Task
+  def run([arg| _]) do
+    Monkex.run(arg)
+  end
+end
